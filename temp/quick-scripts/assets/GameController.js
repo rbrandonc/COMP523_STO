773d4c1ca98512070352aa8660d9391f077ea240
Brandon <r.brandon.c@gmail.com>
2017-09-19 18:43:55 -0700

    First screen done, one button script

---
+++ temp/quick-scripts/assets/GameController.js
@@ -0,0 +1,49 @@
+(function() {"use strict";var __module = CC_EDITOR ? module : {exports:{}};var __filename = 'preview-scripts/assets/GameController.js';var __require = CC_EDITOR ? function (request) {return cc.require(request, require);} : function (request) {return cc.require(request, __filename);};function __define (exports, require, module) {"use strict";
+cc._RF.push(module, 'ad7edagkKVPrq864CUgNTNE', 'GameController', __filename);
+// GameController.js
+
+"use strict";
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,      // The default value will be used only when the component attaching
+        //                           to a node for the first time
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+        "Tool 1": cc.Button
+    },
+
+    // use this for initialization
+    onLoad: function onLoad() {},
+
+    buttonClick: function buttonClick() {
+        console.log("ewrqqwefsadfnasdof");
+    }
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
+
+cc._RF.pop();
+        }
+        if (CC_EDITOR) {
+            __define(__module.exports, __require, __module);
+        }
+        else {
+            cc.registerModuleFunc(__filename, function () {
+                __define(__module.exports, __require, __module);
+            });
+        }
+        })();
+        //# sourceMappingURL=GameController.js.map
+        
