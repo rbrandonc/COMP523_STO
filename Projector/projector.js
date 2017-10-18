a875303f49ccaab22e0d8fd17da62af9e5aebe0d
rbrandonc <r.brandon.c@gmail.com>
2017-10-18 12:40:53 -0400

    map

--- Projector/projector.js
+++ Projector/projector.js
@@ -31,3 +31,63 @@ var send = function(data) {
   data.id = this.id;
   ws.send(JSON.stringify(data));
 }
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    // spread(500)
+    // setTimeout(() => {spread(900)}, 8000);
+    // setTimeout(() => {spread(200)}, 16000);
+    var canvas = document.getElementById('canvas');
+    w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    var canvas = document.getElementById('canvas');
+    canvas.height = h;
+    canvas.width = w;
+
+    var heat = simpleheat(canvas);
+    var data = [];
+
+    for(var j = 1; j < 10; j+=1) {
+      var item = [w*Math.random(),Math.random()*h,0.5];
+      data.push(item)
+    }
+
+    console.log(data);
+    heat.data(data);
+    heat.gradient({0.0: 'blue', 0.5: 'lime', 1: 'red'});
+    heat.resize();
+    heat.radius(10, 20);
+    heat.draw();
+
+    var grow = setInterval(() => {
+      var count = 0;
+
+      for(let point of data) {
+        point[2] += .01;
+      }
+
+      var n = [];
+      for(let point of data) {
+        for(var x = 0; x < 10; x++) {
+          var probability = Math.random();
+          if(probability > 0.5) {
+            n.push([
+              point[0] + (((Math.random()*2)-1)*10),
+              point[1] + (((Math.random()*2)-1)*10),
+              .5]);
+            }
+        }
+      }
+
+      for(let p of n) {
+        data.push(p);
+      }
+
+      count++;
+      if(count > 20) {
+        clearInterval(this);
+      }
+
+      heat.draw();
+
+    }, 300)
+});
